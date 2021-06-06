ac6c2bf915576edd40c6b5e74f7d61fef6c11df4
jordan819 <patryk.zaucha00@gmail.com>
2021-06-06 23:52:57 +0200

    added exchange result display

--- src/main/java/pl/pwsztar/CurrencyConversionController.java
+++ src/main/java/pl/pwsztar/CurrencyConversionController.java
@@ -1,8 +1,45 @@
 package pl.pwsztar;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import pl.pwsztar.Connect.Money;
+
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class CurrencyConversionController {
+
+    public ChoiceBox choiceBox;
+    public Label exchangeDisplay;
+
+    private String currencyBefore;
+    private String currencyAfter;
+
+    @FXML
+    private void initialize() {
+        currencyBefore = App.loggedCustomerAccount.getCurrency();
+
+        choiceBox.getItems().addAll("PLN", "USD", "GBP", "EUR");
+        choiceBox.getSelectionModel().selectedIndexProperty().addListener((observableValue, number, number2) -> {
+
+            currencyAfter = (String) choiceBox.getItems().get((Integer) number2);
+
+            double balanceBefore = App.loggedCustomerAccount.getBalance();
+            double balanceAfter = Money.exchange(balanceBefore, currencyBefore, currencyAfter);
+
+            String result = String.format("%.2f %s -> %.2f %s",
+                    balanceBefore, currencyBefore, balanceAfter, currencyAfter);
+
+            exchangeDisplay.setText(result);
+
+        });
+
+        choiceBox.getSelectionModel().selectFirst();
+    }
+
     public void goBack() throws IOException {
         App.setRoot("accountManage");
     }
