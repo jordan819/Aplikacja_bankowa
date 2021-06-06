db7b2a43969aed101a0bb04e616a385a98590dd8
jordan819 <patryk.zaucha00@gmail.com>
2021-06-06 23:08:15 +0200

    added balance display on the main window

--- src/main/java/pl/pwsztar/AccountManageController.java
+++ src/main/java/pl/pwsztar/AccountManageController.java
@@ -1,9 +1,29 @@
 package pl.pwsztar;
 
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import pl.pwsztar.Connect.Account;
+import pl.pwsztar.Connect.CustomerDto;
+import pl.pwsztar.Connect.Database;
+
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 public class AccountManageController {
+
+    public Label balanceDisplay;
+
+    @FXML
+    private void initialize() {
+        App.loggedCustomerAccount = setAccount(App.loggedCustomer);
+        assert App.loggedCustomerAccount != null;
+        String balance = String.format("%.2f ", App.loggedCustomerAccount.getBalance());
+        balance += App.loggedCustomerAccount.getCurrency();
+        balanceDisplay.setText(balance);
+    }
+
     public void goToCurrencyConversion() throws IOException {
         App.setRoot("currencyConversion");
     }
@@ -30,5 +50,19 @@ public class AccountManageController {
 
     public void goToPayLoan() throws IOException {
         App.setRoot("payLoan");
+    }
+
+    private Account setAccount(CustomerDto customer) {
+        try {
+            List<Account> accounts = Database.fetchAccounts();
+            for (Account account: accounts) {
+                if (account.getAccountId().equals(customer.getIdAccount())) {
+                    return account;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
