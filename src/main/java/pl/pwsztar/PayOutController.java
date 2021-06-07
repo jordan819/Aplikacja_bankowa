e2d2dceb65f1bc741c2095551bd0df1cb5bdddfa
jordan819 <patryk.zaucha00@gmail.com>
2021-06-07 08:54:29 +0200

    added payOut functionality

--- src/main/java/pl/pwsztar/PayOutController.java
+++ src/main/java/pl/pwsztar/PayOutController.java
@@ -1,10 +1,92 @@
 package pl.pwsztar;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import pl.pwsztar.Connect.Database;
+
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class PayOutController {
 
-    public void goBack() throws IOException {
-        App.setRoot("accountManage");
+    public TextField otherInput;
+    public Button btn10;
+    public Button btn20;
+    public Button btn50;
+    public Button btn100;
+    public Button btn200;
+    public Button btn500;
+    public Button otherButton;
+    public Label infoDisplay;
+
+    @FXML
+    private void initialize() {
+        String currency = App.loggedCustomerAccount.getCurrency();
+        btn10.setText("10 " + currency);
+        btn20.setText("20 " + currency);
+        btn50.setText("50 " + currency);
+        btn100.setText("100 " + currency);
+        btn200.setText("200 " + currency);
+        btn500.setText("500 " + currency);
+
+        otherInput.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                otherInput.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+            otherButton.disableProperty().set(!(Integer.parseInt(otherInput.getText())%10==0));
+        });
+
+    }
+
+    public void payOut10() {
+        payOut("10");
+    }
+
+    public void payOut20() {
+        payOut("20");
+    }
+
+    public void payOut50() {
+        payOut("10");
+    }
+
+    public void payOut100() {
+        payOut("100");
+    }
+
+    public void payOut200() {
+        payOut("200");
+    }
+
+    public void payOut500() {
+        payOut("500");
+    }
+
+    public void payOutOther() {
+        payOut(otherInput.getText());
+    }
+
+    private void payOut(String value) {
+        if (balanceSufficient(Double.parseDouble(value))) {
+            Database.updateAccountBalance(App.loggedCustomerAccount.getAccountId(), "-" + value);
+            goBack();
+        } else {
+            infoDisplay.setVisible(true);
+            infoDisplay.setText("Brak środków na koncie!");
+        }
+    }
+
+    private boolean balanceSufficient(double amount) {
+        return App.loggedCustomerAccount.getBalance() >= amount;
+    }
+
+    public void goBack() {
+        try {
+            App.setRoot("accountManage");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
